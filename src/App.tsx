320034774ac694c63ade59b42e45e84ca631ffd9
João Lee <joaumlimaum@gmail.com>
2024-07-15 16:57:29 -0300

    Create typewriter animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 
-import './App.css'
-import { Header } from './components/Header/Header'
+import { Header } from './components/Header/Header';
+import './App.css';
+import Greeting from './assets/components/Greeting/Greeting';
 
 function App() {
   return (
-    // <div>
     <>
-      {/* <Header /> */}
-      <div className="typewriter">
-        <h1>Hi, my name is João!</h1>
-        <h2>I'm a full stack web developer!</h2>
-        <h3>Welcome to my portfolio.</h3>
-      </div>
+      <Header />
+      <Greeting />
     </>
   )
-    // </div>
+
 }
 
 export default App
---
+++ src/assets/components/Greeting/Greeting.tsx
@@ -0,0 +1,10 @@
+function Greeting() {
+  return (
+    <div className="typewriter">
+      <h1>Hi, my name is João!</h1>
+      <p className='subtitle'>Welcome to my portfolio.</p>
+    </div>
+  )
+}
+
+export default Greeting;
