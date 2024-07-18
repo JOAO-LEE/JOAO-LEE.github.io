c28dacb35e8377555718a3471d3b121210043a0e
João Lee <joaumlimaum@gmail.com>
2024-07-18 18:32:52 -0300

    Create job description underline animation

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,13 +3,13 @@ import './Intro.css';
 function Intro() {
   return (
     <section 
-    className="flex flex-col lg:flex-row items-center justify-center  gap-0.5 p-2 lg:p-6 intro">
+    className="flex flex-col xl:flex-row items-center justify-center  gap-0.5 p-2 lg:p-6 intro">
       <div className="text-intro flex flex-col gap-2 p-2 greeting-container">
         <h1 className="text-3xl lg:text-5xl">João Lima</h1>
         <h1
-        className="text-4xl lg:text-6xl flex-1 text-wrap uppercase">Full Stack Web Developer</h1>
+        className="uppercase">Full Stack Web Developer</h1>
         <h1 className="text-3xl lg:text-4xl">based in Brazil.</h1>
-        <p className="text-lg">I love creating </p>
+        <p className="text-lg mini-about">Passionate for learning modern technologies and creating clean applications with.</p>
       </div>
       <div
       className="greeting-image">
