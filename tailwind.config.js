2a8b77f13ab92addb9f3ef0e6b4795ba93f21427
João Lee <joaumlimaum@gmail.com>
2024-07-23 10:57:01 -0300

    Add color pallette to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-blue": "#27187E",
+        "dark-lilac": "#758BFD",
+        lilac: "#AEB8FE",
+        whitey: "#F1F2F6",
+        orangeish: "#FF8600",
+        "clear-orangeish": "#FB8500",
+      },
+    },
   },
   plugins: [],
 };
