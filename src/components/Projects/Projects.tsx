b3863e910fa822d54a4b4df1310b2e133178c9f3
João Lee <joaumlimaum@gmail.com>
2024-07-29 20:11:55 -0300

    Projects title and scrap files

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,28 +1,13 @@
+import useIntersectionObserver from '../../hooks/useIntersectionObserver';
 import './Projects.css';
-// flex justify-between items-center
-function Projects() {
-  return (
-    <section className='bg-darkest-purple-blue'>
-      <h2>
-        <span>!r$eC@s</span>
-        <span>P@ojvw$</span>
-        <span>pr#QweY</span>
-        <span>p10j3c75</span>
-        <span>emvwpxvh</span>
-        <span>!r*j3%t@</span>
-        <span>(r#J&Çtx)</span>
-        <span>+r0j3ct=</span>
-        <span>?r@jec7s</span>
-        <span>@r0j3kt$</span>
-        <span>%o*ec@s</span>
-        {/* <span>projects</span> */}
 
-      </h2>
-      {/* <ul className="projects">
-        <li className="project">X Clone</li>
-        <li className="project">Google Clone</li>
-        <li className="project">lamusiq</li>
-      </ul> */}
+function Projects() {
+  const [ref, isVisible] = useIntersectionObserver({threshold: 0.5})
+  return (
+    <section 
+    ref={ref} 
+    className='bg-purple-blue flex justify-center projects-container'>
+      <h2 className={`${isVisible ? 'projects-title' : "text-transparent"}`}>projects</h2>
     </section>
   )
 }
