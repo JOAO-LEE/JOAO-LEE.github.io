5da706588355036028394cf83d0bfd0d52fb4dad
João Lee <joaumlimaum@gmail.com>
2024-08-05 19:35:18 -0300

    Fix projects title animation

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -19,30 +19,26 @@ function Projects() {
   return (
     <section 
     ref={ref} 
-    className='bg-dark-purple-blue flex flex-col items-center gap-8 relative'>
-      <h2 className={`${isVisible ? 'projects-title' : "text-transparent"}`}>projects</h2>
-      <div className={`${isVisible ? "github-stats" : ""} space-y-2`}>
-        <h3 className="text-center">GITHUB STATS</h3>
-        <div className={`flex items-center gap-4 text-xs lg:text-sm `}>
-          <p>public repos <span className='text-clear-orangeish font-semibold'>{githubStats?.public_repos}</span></p>
-          <p>followers <span className='text-clear-orangeish font-semibold'>{githubStats?.followers}</span></p>
-          <p>following <span className='text-clear-orangeish font-semibold'>{githubStats?.following}</span></p>
+    className='bg-darkest-purple-blue'>
+      <div className='flex flex-col items-center'>
+        <h2 className={`${isVisible ? 'projects-title' : "text-transparent"}`}>projects</h2>
+        <div className={`${isVisible ? "github-stats" : ""} space-y-2`}>
+          <h3 className="text-center">GITHUB STATS</h3>
+          <div className={`flex items-center gap-4 text-xs lg:text-sm `}>
+            <p>public repos <span className='text-clear-orangeish font-semibold'>{githubStats?.public_repos}</span></p>
+            <p>followers <span className='text-clear-orangeish font-semibold'>{githubStats?.followers}</span></p>
+            <p>following <span className='text-clear-orangeish font-semibold'>{githubStats?.following}</span></p>
+          </div>
         </div>
-      </div>
-      <div className='grow'>
-      <h4>see my recent work</h4>
-      </div>
-      <div className={`${!isVisible ? "hidden" : "block"} self-end p-2 `}>
-        <GithubLogo weight='light' size={"2rem"} />
+        <div className='grow'>
+        <h4>see my recent work</h4>
+        </div>
+        <div className={`${!isVisible ? "hidden" : "block"} self-end p-2 `}>
+          <GithubLogo weight='light' size={"2rem"} />
+        </div>
       </div>
     </section>
   )
 }
-
-{/* <p>public repositories <span className='text-clear-orangeish'>{githubStats?.public_repos}</span></p>
-          <p>followers  <span className='text-clear-orangeish'>{githubStats?.followers}</span></p>
-          <p>following <span className='text-clear-orangeish'>{githubStats?.following}</span></p> */}
-
-// {!isVisible ? 'hidden self-end p-2}`
 
 export default Projects;
