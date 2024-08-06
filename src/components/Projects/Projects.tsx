1ef5d04aa73e8bffacb17fe1709269400246a953
João Lee <joaumlimaum@gmail.com>
2024-08-05 21:27:44 -0300

    Fix projects animation

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -19,23 +19,23 @@ function Projects() {
   return (
     <section 
     ref={ref} 
-    className='bg-darkest-purple-blue'>
-      <div className='flex flex-col items-center'>
+    className='bg-purple-blue'>
+      <div className='flex flex-col items-center gap-8 h-full'>
         <h2 className={`${isVisible ? 'projects-title' : "text-transparent"}`}>projects</h2>
         <div className={`${isVisible ? "github-stats" : ""} space-y-2`}>
           <h3 className="text-center">GITHUB STATS</h3>
-          <div className={`flex items-center gap-4 text-xs lg:text-sm `}>
+          <div className={`flex items-center gap-4 text-xs lg:text-sm`}>
             <p>public repos <span className='text-clear-orangeish font-semibold'>{githubStats?.public_repos}</span></p>
             <p>followers <span className='text-clear-orangeish font-semibold'>{githubStats?.followers}</span></p>
             <p>following <span className='text-clear-orangeish font-semibold'>{githubStats?.following}</span></p>
           </div>
         </div>
-        <div className='grow'>
-        <h4>see my recent work</h4>
+        <div className='flex-1'>
+          <h4>see my recent work</h4>
         </div>
-        <div className={`${!isVisible ? "hidden" : "block"} self-end p-2 `}>
-          <GithubLogo weight='light' size={"2rem"} />
-        </div>
+      <div className={`${isVisible ? "github-logo" : ""} self-end content-end p-2`}>
+        <GithubLogo weight='light' size={"2rem"} />
+      </div>
       </div>
     </section>
   )
