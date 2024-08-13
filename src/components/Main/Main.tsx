10994e753cb2d2f983360b9615f67d88bdd8ede4
João Lee <joaumlimaum@gmail.com>
2024-08-13 11:17:26 -0300

    Change Intro colors

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import './Main.css';
 
-function Main({children}: {children: ReactNode}) {
+function Main({ children }: { children: ReactNode }) {
   return (
     <main className="main-content">
       {children}
