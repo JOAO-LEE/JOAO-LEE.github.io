fddfb1c46fb2090b7badc8a1e03a8e432ae54cf7
João Lee <joaumlimaum@gmail.com>
2024-08-22 18:59:11 -0300

    Rename assets folder

--- src/projects.ts
+++ src/projects.ts
@@ -6,7 +6,7 @@ export const projects: Array<Project> = [
     name: "X Clone",
     githubRepository: "https://github.com/JOAO-LEE/x_clone",
     description: "X's (former Twitter) replica. Users can create, like, reply and delete posts of their own. It's also possible to add images to a post. Currently requires a Google Account to use.",
-    image: "src/assets/x-clone.png",
+    image: "src/images/x-clone.png",
     deployed: true,
     applicationUrl: "https://x-clone-tan-seven.vercel.app/",
     techStack: ["Next.js", "React.js", "TypeScript", "TailwindCSS", "Firebase"]
@@ -16,7 +16,7 @@ export const projects: Array<Project> = [
     name: "Google Clone",
     githubRepository: "https://github.com/JOAO-LEE/google_clone",
     description: "",
-    image: "src/assets/google-clone.png",
+    image: "src/images/google-clone.png",
     deployed: true,
     applicationUrl: "https://google-clone-eta-henna.vercel.app/",
     techStack: ["Next.js", "React.js", "TypeScript", "TailwindCSS", "Firebase"]
